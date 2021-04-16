5e7f2fab760547031bd6bc3dd61b5967021dbf55
VSCS <you@example.com>
2021-04-16 11:34:58 -0300

    Projeto Pronto Eletrônico

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Carbon\Carbon;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'      => 'Colaborador',
+            'email'     => 'teste@mail.com',
+            'password'  => bcrypt('123456789'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        User::create([
+            'name'      => 'Admin',
+            'email'     => 'admin@mail.com',
+            'role_id'     => '2',
+            'password'  => bcrypt('123456789'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    }
+}
